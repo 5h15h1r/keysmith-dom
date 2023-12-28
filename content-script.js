956de21a254f67c5ada3a68e037b94058e405844
Shishir Agrahari <shishirrocks1420@gmail.com>
2023-12-28 16:06:27 +0530

    Dist folder for keysmith extension

---
+++ content-script.js
@@ -0,0 +1,59 @@
+        const userName = document.getElementsByTagName("input")
+window.onload = () =>{
+    // console.log(userName.value);
+}
+
+
+let showPopup = false
+function handleFormSubmission(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    // Access form data
+    var formData = new FormData(event.target);
+  
+    // Convert FormData to a plain object
+    var formDataObject = {};
+    formData.forEach(function(value, key){
+      formDataObject[key] = value;
+    });
+    
+
+   showPopup = true
+    chrome.runtime.sendMessage({bool : showPopup})
+    console.log(showPopup);
+    saveFormData(formDataObject);
+// Sample data to send to the background script
+}
+
+
+function saveFormData(formData) {
+    // Get existing data from storage (if any)
+    chrome.storage.local.get({ savedFormData: [] }, function (result) {
+      var savedFormDataArray = result.savedFormData;
+  
+      // Add the new form data to the array
+      savedFormDataArray.push(formData);
+  
+      // Save the updated array back to storage
+      chrome.storage.local.set({ savedFormData: savedFormDataArray }, function () {
+       
+      });
+    });
+  }
+
+
+document.addEventListener('submit', handleFormSubmission, true);
+
+
+
+function retrieveSavedFormData() {
+    chrome.storage.local.get({ savedFormData: [] }, function (result) {
+      var savedFormDataArray = result.savedFormData;
+      
+      console.log("Retrieved form data from storage:", savedFormDataArray);
+    });
+  }
+  
+  // Call the function to retrieve saved form data
+  retrieveSavedFormData();
+
